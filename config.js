23bcf8fbf8da1c99d4e09a9e009b3193d77b0760
ZΞГ҉̷҉O̸ <112828300+ZERO-Official-0@users.noreply.github.com>
2025-02-18 14:25:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,21 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
+    },
+    {
       name: "Koi Node V4",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
+    {
+      name: "INZEWORLD.COM (DE)",
       password: "saher.inzeworld.com",
       host: "lava.inzeworld.com",
       port: 3128,
