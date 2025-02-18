d51e5aadf510169ca6fdd03f80e39a050d177ecc
ZΞГ҉̷҉O̸ <112828300+ZERO-Official-0@users.noreply.github.com>
2025-02-18 14:33:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1016060774944346142", ""], 
-  mongodbUri : "mongodb+srv://zhakceeer:9OZ5rpX6SEOdICAS@zero.abnpr.mongodb.net/?retryWrites=true&w=majority&appName=ZERO",
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
